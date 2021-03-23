253540e17034f5d8b43657c9eb9165a1ff4b4aa6
saranyanambiarr <sharanya8095@gmail.com>
2021-03-23 09:29:49 +0530

    added prod fn in br1

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -4,7 +4,19 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
-		System.out.println("n");
+		add(10,20);
+	}
+	
+	public static void add(int num1, int num2){
+		System.out.println(num1+num2);
+	}
+	
+	public static void diff(int num1, int num2){
+		System.out.println(num1-num2);
+	}
+	
+	public static void prod(int num1, int num2){
+		System.out.println(num1*num2);
 	}
 
 }
